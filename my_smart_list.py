a6887431eeebf05ae47bacde0fe18dd0b017c257
Chris Sabb <87588206+real-csabb@users.noreply.github.com>
2021-08-25 16:38:54 -0400

    Added pseudocode for upcoming call and remove features.

--- my_smart_list.py
+++ my_smart_list.py
@@ -6,6 +6,12 @@ while True:
     i = input("Enter something! Anything!: ")
     if i == "Exit" or i == "exit":
         break
+    """elif function that checks for "call" and
+            then gets the index number provided.
+    """
+    """ elif function that checks for "Remove" and
+            then removes index number or value provided.
+    """
     else:
         my_smart_list.append(i)
         print(my_smart_list)
