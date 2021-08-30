b77798827c6ab3d9e686991e146636af53757ee7
Chris Sabb <87588206+real-csabb@users.noreply.github.com>
2021-08-29 22:03:46 -0400

    Removed redundancy by omitting $10 bills and dimes, and debugged it by accounting for inputs less than or equal to zero.

--- Counting Change/script.py
+++ Counting Change/script.py
@@ -2,10 +2,15 @@
 # IS LESS THAN PURCHASE TOTAL, IT PROMPTS THE USER TO GIVE CASH GREATER THAN THE TOTAL DUE.
 
 purchase = float(input("Please enter the total due on the purchase: "))
+if purchase <= 0:
+    print("Please enter a number greater than zero.")
+    while purchase <= 0:
+        try: purchase = float(input(": "))
+        except: print("Invalid input.")
 given = float(input("Please also enter the cash given for the purchase: "))
 if given < purchase:
     print("Please give cash greater than the purchase total.")
-    while given < purchase:
+    while given < purchase or given <= 0:
         try: given = float(input(": "))
         except: print("Invalid input.")
 change = given - purchase
@@ -20,9 +25,6 @@ change = change % 50
 if (change // 20) >= 1: print(int(change // 20), "$20 Bills.")
 change = change % 20
 
-if (change // 10) >= 1: print(int(change // 10), "$10 Bills.")
-change = change % 5
-
 if (change // 5) >= 1: print(int(change // 5), "Fives.")
 change = change % 5
 
@@ -34,9 +36,6 @@ change = round(change * 100)
 
 if (change // 25) >= 1: print(int(change // 25), "Quarters.")
 change = change % 25
-
-if (change // 10) >= 1: print(int(change // 10), "Dimes.")
-change = change % 10
 
 if (change // 5) >= 1: print(int(change // 5), "Nickels.")
 change = change % 5
