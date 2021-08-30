54c2977ee2a1db279a4c975a60edec81695e2b7f
Chris Sabb <87588206+real-csabb@users.noreply.github.com>
2021-08-29 21:48:35 -0400

    Simplified my code by removing the elif statements and simplfying the remaining if statements.

--- Counting Change/script.py
+++ Counting Change/script.py
@@ -1,5 +1,3 @@
-# SOLUTION USING IF/ELIF/ELSE
-
 # ALGORITHM THAT GETS PURCHASE TOTAL AND GIVEN CASH, AND CALCULATES CHANGE DUE. IF GIVEN AMOUNT
 # IS LESS THAN PURCHASE TOTAL, IT PROMPTS THE USER TO GIVE CASH GREATER THAN THE TOTAL DUE.
 
@@ -12,38 +10,36 @@ if given < purchase:
         except: print("Invalid input.")
 change = given - purchase
 
-# FOR DEBUGGING PURPOSES
-debugger = change # I know I don't have to do this, but I just wanted to do.
-print("---\n", f"Change due is {debugger}", end="\n---\n")
-change = debugger
-
-# ALGORTHM PROPER--COUNTING CHANGE ALGORITHM
-
-# Starting banknotes.
-print("Change given: ", end="")
-
-if (change // 100) > 1: print(int(change // 100), "hundreds", end=", ")
-elif (change // 100) == 1: print("An one (1) hundred", end=", ")
+# CASH
+if (change // 100) >= 1: print((change // 100), "$100 Bills.")
 change = change % 100
 
-if (change // 50) == 1 or change == 50: print("one (1) fifty", end=", ")
+if (change // 50) >= 1: print((change // 50), "$50 Bills.")
 change = change % 50
 
-if (change // 20) > 1: print(int(change // 20), "twenties", end=", ")
-elif (change // 20) == 1: print("one (1) twenty", end=", ") 
+if (change // 20) >= 1: print(int(change // 20), "$20 Bills.")
 change = change % 20
-    
-if (change // 10) == 1 or change == 10: print("one (1) ten", end=", ")
-change = change % 10
 
-if (change // 5) == 1 or change == 5: print("one (1) five", end=", and ")
+if (change // 10) >= 1: print(int(change // 10), "$10 Bills.")
 change = change % 5
 
-if (change // 1) > 1: print(int(change // 1), "ones", end=" in cash. ")
-elif (change // 1) == 1: print("an one (1) in cash", end=". ")
+if (change // 5) >= 1: print(int(change // 5), "Fives.")
+change = change % 5
+
+if (change // 1) >= 1: print(int(change // 1), "Dollar Bills.")
 change = change % 1
-    
-# Finally, coins.
-change = change * 100
-if (change // 25) > 1: print("And in change, ", int(change // 25), "quarters,", end=", ")
-elif (change // 25) == 1: print("And in change, one (1) quarter", end=", and")
+
+# CHANGE
+change = round(change * 100)
+
+if (change // 25) >= 1: print(int(change // 25), "Quarters.")
+change = change % 25
+
+if (change // 10) >= 1: print(int(change // 10), "Dimes.")
+change = change % 10
+
+if (change // 5) >= 1: print(int(change // 5), "Nickels.")
+change = change % 5
+
+if (change // 1) >= 1: print(int(change // 1), "Pennies.")
+change = change % 1
